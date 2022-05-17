864b2eb76139bb948711ec3d326e2f720344c110
Mycroft <kaseyfitton@hotmail.co.uk>
2022-05-17 20:44:51 +0100

    add import

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,10 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_script 'config.lua'
+shared_scripts {
+	'@es_extended/imports.lua',
+	'config.lua'
+}
 
 client_script 'client/main.lua'
 server_scripts {
